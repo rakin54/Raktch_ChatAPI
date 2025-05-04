47e4b29ceb1c72fb857431ae6ca81941ca17b3ee
rakinplaban <rakinshahriar54@gmail.com>
2025-05-04 14:23:18 +0600

    feat: Delete and Update done.

--- blog/blogapi/urls.py
+++ blog/blogapi/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('api/register/', RegisterView.as_view(), name='register'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/posts/', PostView.as_view(), name='posts'),
     path('api/posts/<int:id>', PostDetailView.as_view(), name='posts_details'),
-
 ]
--- blog/blogapi/views.py
+++ blog/blogapi/views.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
-
+from rest_framework.exceptions import PermissionDenied
 from .serializers import RegisterSerializer, PostSerializer
 from .models import Post
 
@@ -37,9 +37,31 @@ class PostView(APIView):
 
 
 class PostDetailView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly] 
+
+    def get_object(self, id):
+        return get_object_or_404(Post, id=id)
+    
     def get(self, request, id):
-        post = get_object_or_404(Post, id=id)
+        post = self.get_object(id)
         serializer = PostSerializer(post)
         return Response(serializer.data)
+    
+    def put(self, request, id):
+        post = self.get_object(id)
+        if request.user != post.author:
+            raise PermissionDenied("You do not have permission to edit this post.")
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        post = self.get_object(id)
+        if request.user != post.author:
+            raise PermissionDenied("You do not have permission to delete this post.")
+        post.delete()
+        return Response({"message": "Post deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
 
     
